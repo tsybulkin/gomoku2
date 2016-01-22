d3da68808eee006f42268340aa3b9b2317c5ba97
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2016-01-22 19:50:38 +0200

    logging W values into the file implemented

--- src/evil_bot_learning.erl
+++ src/evil_bot_learning.erl
@@ -7,7 +7,7 @@
 %   different feature coefficients. Depending on who wins the algorithm
 %   modifies the coefficients in accordance with gradient between two agents.
 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -module(evil_bot_learning).
 -export([run/4
@@ -16,9 +16,11 @@
 
 
 
-run(Nbr_agents,Nbr_matches,Feat1,Feat2) ->
+run(Nbr_agents,Nbr_matches,{F1,_}=Feat1,{F2,_}=Feat2) ->
 	Coeffs = [ evil_bot:init_w() || _ <- lists:seq(1,Nbr_agents)],
 	Stat_file = "data/stat_file.dat",
+	file:write_file(Stat_file,"Features: "++atom_to_list(F1)++"-"++atom_to_list(F2)++"\n"),
+	file:write_file(Stat_file,Feat1,[append]),
 	run_matches(Nbr_matches,Coeffs,Stat_file,Feat1,Feat2).
 
 
@@ -26,8 +28,6 @@ run_matches(0,_Coeffs,Stat_file,_,_) ->
 	io:format("All matches done. Results are in ~p file~n",[Stat_file]);
 
 run_matches(Nbr_matches,Coeffs,Stat_file,Feat1,Feat2) ->
-	log_features(Stat_file,Coeffs,Feat1,Feat2),
-
 	[W1|Coeff1] = Coeffs,
 	{L1,[W2|L2]} = lists:split(random:uniform(length(Coeff1)-1),Coeff1),
 
@@ -41,13 +41,32 @@ run_matches(Nbr_matches,Coeffs,Stat_file,Feat1,Feat2) ->
 	{Res1,{_,_,_,_,_,W11},{_,_,_,_,_,W22}} = game:run(State,evil_bot,E1,evil_bot,E2),
 	{Res2,_,_} = game:run(State,evil_bot,{given_W,W22},evil_bot,{given_W,W11}),
 	Coeffs2 = case {Res1,Res2} of
-		{Same,Same} ->             L1++L2++[W11,W22];
-		{blacks_won,whites_won} -> L1++L2++[W11,get_w2(W11,W22,lost_lost)];
-		{whites_won,blacks_won} -> L1++L2++[W22,get_w2(W22,W11,lost_lost)];
-		{blacks_won,draw} ->       L1++L2++[W11,get_w2(W11,W22,lost_draw)];
-		{whites_won,draw} ->	   L1++L2++[W22,get_w2(W22,W11,lost_draw)];
-		{draw,blacks_won} ->	   L1++L2++[W22,get_w2(W22,W11,lost_draw)];
-		{draw,whites_won} ->	   L1++L2++[W11,get_w2(W11,W22,lost_draw)]
+		{Same,Same} -> 
+			L1++L2++[W11,W22];
+		{blacks_won,whites_won} -> 
+			W=get_w2(W11,W22,lost_lost),
+			log_feature(Stat_file,Feat1,Feat2,W22,W),
+			L1++L2++[W11,W];
+		{whites_won,blacks_won} -> 
+			W=get_w2(W22,W11,lost_lost),
+			log_feature(Stat_file,Feat1,Feat2,W11,W),
+			L1++L2++[W22,W];
+		{blacks_won,draw} ->  
+			W=get_w2(W11,W22,lost_draw),   
+			log_feature(Stat_file,Feat1,Feat2,W22,W),  
+			L1++L2++[W11,W];
+		{whites_won,draw} ->
+			W=get_w2(W22,W11,lost_draw),
+			log_feature(Stat_file,Feat1,Feat2,W11,W),	   
+			L1++L2++[W22,W];
+		{draw,blacks_won} ->
+			W=get_w2(W22,W11,lost_draw),	   
+			log_feature(Stat_file,Feat1,Feat2,W11,W),
+			L1++L2++[W22,W];
+		{draw,whites_won} ->
+			W=get_w2(W11,W22,lost_draw), 
+			log_feature(Stat_file,Feat1,Feat2,W22,W),
+			L1++L2++[W11,W]
 	end,
 	run_matches(Nbr_matches-1,Coeffs2,Stat_file,Feat1,Feat2).
 
@@ -64,5 +83,8 @@ get_w2(Won,Lost,lost_draw) ->
 
 
 
-log_features(File,Coeffs,Feat1,Feat2) -> ok.
+log_feature(Stat_file,Feat1,Feat2,W_old,W_new) ->
+	Old = float_to_list(dict:fetch(Feat1,W_old))++" "++float_to_list(dict:fetch(Feat2,W_old)),
+	New = float_to_list(dict:fetch(Feat1,W_new))++" "++float_to_list(dict:fetch(Feat2,W_new)),
+	file:write_file(Stat_file,Old++" "++New++"\n",[append]).
 
